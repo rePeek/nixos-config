40e32c31d11a4dc871e0bd4b1a2cced504e993bc
rePeek <senxlin@gmail.com>
2026-03-24 21:57:44 +0800

    refactor: refactor nix config

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,12 +1,21 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
+  nixpkgs.config.allowUnfree = lib.mkForce true;
+
   nix.settings = {
+    # Manual optimise storage: nix-store --optimise
+    # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
     auto-optimise-store = true;
     # enable flakes globally
     experimental-features = [
       "nix-command"
       "flakes"
     ];
+
+    # given the users in this list the right to specify additional substituters via:
+    #    1. `nixConfig.substituers` in `flake.nix`
+    #    2. command line args `--options substituers http://xxx`
+    # trusted-users = [ myvars.username ];
 
     substituters = [
       # cache mirror located in China
@@ -19,6 +28,7 @@
     ];
 
     trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
     builders-use-substitutes = true;
@@ -31,8 +41,6 @@
     options = lib.mkDefault "--delete-older-than 7d";
   };
 
-  environment.systemPackages = with pkgs; [
-    nix-output-monitor
-    nvd
-  ];
+  # remove nix-channel related tools & configs, we use flakes instead.
+  nix.channel.enable = false;
 }
