26396fa30016141ea84214e45afd690a106964cc
rePeek <senxlin@gmail.com>
2026-03-25 13:34:05 +0800

    refactor: refact pipeware

--- modules/nixos/extraServices/desktop/pipewire.nix
+++ modules/nixos/extraServices/desktop/pipewire.nix
@@ -1,12 +1,28 @@
 { pkgs, ... }:
 {
-  services.pulseaudio.enable = false;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # provides `pactl`, which is required by some apps(e.g. sonic-pi)
+    pulseaudio
+  ];
+
+  # PipeWire is a new low-level multimedia framework.
+  # It aims to offer capture and playback for both audio and video with minimal latency.
+  # It support for PulseAudio-, JACK-, ALSA- and GStreamer-based applications.
+  # PipeWire has a great bluetooth support, it can be a good alternative to PulseAudio.
+  #     https://nixos.wiki/wiki/PipeWire
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
     jack.enable = true;
+    wireplumber.enable = true;
   };
-  environment.systemPackages = with pkgs; [ pulseaudioFull ];
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  # Disable pulseaudio, it conflicts with pipewire too.
+  services.pulseaudio.enable = false;
 }
