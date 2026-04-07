dc8793b0db43fc3fe18652a0da0e67a42719a97f
rePeek <senxlin@gmail.com>
2026-04-07 11:01:33 +0800

    feat: disable daed && use clash

--- hosts/brain-holder/default.nix
+++ hosts/brain-holder/default.nix
@@ -5,13 +5,14 @@
     ./user.nix
     ./misc.nix
     ./network.nix
+    ./clash-verge.nix
 
     ../../modules/nixos
     ../../modules/nixos/extraServices
   ];
 
   modules.desktop.gaming.enable = true;
-  modules.network.daed.enable = true;
+  # modules.network.daed.enable = true;
   modules.virtualization.custom.docker = true;
   # modules.powerManagement.type = "workstation";
 }
--- modules/home-manager/llm-agents-package.nix
+++ modules/home-manager/llm-agents-package.nix
@@ -4,6 +4,7 @@
   home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
     crush
     claude-code
+    codex
     beads
   ];
 }
