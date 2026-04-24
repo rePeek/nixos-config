6191e7343c0c400375855a77277e308860d45e89
rePeek <senxlin@gmail.com>
2026-04-24 20:33:01 +0800

    Revert "feat: use wifi as default"
    This reverts commit 6957359bbc7f487a7e4bfe7a9e01bed548e344be.

--- hosts/brain-holder/network.nix
+++ hosts/brain-holder/network.nix
@@ -2,27 +2,7 @@
 {
   networking = {
     hostName = "brain-holder";
-    networkmanager = {
-      enable = true;
-      # 这里使用自定义网口的原因是这个网口连着旁路由
-      # 会导致 tailscale 网内连接变慢
-      # 本机上已自动开启了 clash
-      ensureProfiles.profiles."ethernet-no-default" = {
-        connection = {
-          id = "ethernet-no-default";
-          type = "ethernet";
-          interface-name = "enp5s0";
-          autoconnect = true;
-        };
-
-        ipv4 = {
-          method = "auto";
-          never-default = true;
-        };
-
-        ipv6.method = "disabled";
-      };
-    };
+    networkmanager.enable = true;
     nameservers = [
       "8.8.8.8"
       "8.8.4.4"
