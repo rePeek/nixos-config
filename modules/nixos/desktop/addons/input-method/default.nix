55016be41890c1329e39afd30ca03e6a84a05e14
rePeek <senxlin@gmail.com>
2026-06-09 18:36:30 +0800

    fix: add fcitx input method icons

--- modules/nixos/desktop/addons/input-method/default.nix
+++ modules/nixos/desktop/addons/input-method/default.nix
@@ -7,6 +7,33 @@
 let
   cfg = config.custom.desktop.addons.fcitx5;
   desktopUsers = config.custom.desktop.users;
+  fcitx5ChineseAddons = pkgs.qt6Packages.fcitx5-chinese-addons;
+  # Chinese Addons metadata uses legacy icon names like fcitx-pinyin, while
+  # the package installs namespaced files like org.fcitx.Fcitx5.fcitx-pinyin.
+  fcitx5ChineseAddonIconLinks = pkgs.runCommand "fcitx5-chinese-addon-icon-links" { } ''
+    icon_dir="${fcitx5ChineseAddons}/share/icons"
+
+    for icon in "$icon_dir"/hicolor/*/apps/org.fcitx.Fcitx5.*; do
+        [ -e "$icon" ] || continue
+
+        rel_path="''${icon#"$icon_dir"/}"
+        dir="$out/share/icons/$(dirname "$rel_path")"
+        name="$(basename "$icon")"
+        legacy_name="''${name#org.fcitx.Fcitx5.}"
+
+        mkdir -p "$dir"
+        ln -s "$icon" "$dir/$legacy_name"
+    done
+  '';
+  fcitx5KeyboardIconLinks = pkgs.runCommand "fcitx5-keyboard-icon-links" { } ''
+    icon_dir="$out/share/icons/hicolor/scalable/apps"
+    mkdir -p "$icon_dir"
+
+    ln -s ${pkgs.adwaita-icon-theme}/share/icons/Adwaita/scalable/devices/input-keyboard.svg \
+      "$icon_dir/input-keyboard.svg"
+    ln -s ${pkgs.adwaita-icon-theme}/share/icons/Adwaita/symbolic/devices/input-keyboard-symbolic.svg \
+      "$icon_dir/input-keyboard-symbolic.svg"
+  '';
 
   fcitx5UserModule = {
     home.sessionVariables = {
@@ -16,15 +43,21 @@ let
       XMODIFIERS = lib.mkDefault "@im=fcitx";
     };
 
+    home.packages = [
+      fcitx5ChineseAddons
+      fcitx5ChineseAddonIconLinks
+      fcitx5KeyboardIconLinks
+    ];
+
     i18n.inputMethod = {
       enable = lib.mkDefault true;
       type = lib.mkDefault "fcitx5";
       fcitx5 = {
         waylandFrontend = lib.mkDefault true;
-        addons = with pkgs; [
-          qt6Packages.fcitx5-configtool
-          qt6Packages.fcitx5-chinese-addons
-          fcitx5-gtk
+        addons = [
+          pkgs.qt6Packages.fcitx5-configtool
+          fcitx5ChineseAddons
+          pkgs.fcitx5-gtk
         ];
         settings = {
           inputMethod = {
