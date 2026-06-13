90aa49466e49be09ac19452ac60158993821efe8
rePeek <senxlin@gmail.com>
2026-06-13 17:12:58 +0800

    feat: theme root home-manager with stylix

--- hosts/nixos-in-docker/root.nix
+++ hosts/nixos-in-docker/root.nix
@@ -1,4 +1,7 @@
 { ... }:
+let
+  base16Schemes = import ../../modules/theme/base16-schemes.nix;
+in
 {
   imports = [
     ../../modules/home-manager/common
@@ -7,6 +10,26 @@
 
   home.username = "root";
   home.homeDirectory = "/root";
+
+  stylix = {
+    enable = true;
+    autoEnable = false;
+    base16Scheme = base16Schemes.wolf-alabaster-dark;
+    polarity = "dark";
+
+    targets = {
+      bat.enable = true;
+      btop.enable = true;
+      fish.enable = true;
+      fzf.enable = true;
+      helix.enable = true;
+      lazygit.enable = true;
+      nushell.enable = true;
+      starship.enable = true;
+      yazi.enable = true;
+      zellij.enable = true;
+    };
+  };
 
   programs.git.settings.user = {
     name = "rePeek";
