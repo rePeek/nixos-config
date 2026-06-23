e8a4447a19d9ab79105dc608d1a679a20d759040
rePeek <senxlin@gmail.com>
2026-06-23 12:33:24 +0800

    fix(home): migrate stale kvantum theme link

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -26,7 +26,22 @@ in
     ./dms
   ];
 
-  config.home.packages = lib.mkIf config.custom.desktop.enable [
-    pkgs.wechat
-  ];
+  config = lib.mkIf config.custom.desktop.enable {
+    home = {
+      packages = [
+        pkgs.wechat
+      ];
+
+      activation.migrateKvantumBase16Theme = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+        themeDir="${config.xdg.configHome}/Kvantum/Base16Kvantum"
+
+        if [ -L "$themeDir" ]; then
+          target="$(readlink "$themeDir")"
+          if [[ "$target" == /nix/store/*-home-manager-files/.config/Kvantum/Base16Kvantum ]]; then
+            run rm "$themeDir"
+          fi
+        fi
+      '';
+    };
+  };
 }
