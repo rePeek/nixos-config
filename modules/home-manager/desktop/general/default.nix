1c19070e1443985c3b62a81f7e4148012bd9c163
rePeek <senxlin@gmail.com>
2026-08-08 14:43:20 +0800

    refactor: extract wechat home module

--- modules/home-manager/desktop/general/default.nix
+++ modules/home-manager/desktop/general/default.nix
@@ -2,43 +2,8 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
-let
-  wechatAppimageTools = pkgs.appimageTools // {
-    wrapAppImage =
-      args:
-      pkgs.appimageTools.wrapAppImage (
-        args
-        // {
-          extraPkgs =
-            appPkgs:
-            (args.extraPkgs or (_: [ ])) appPkgs
-            ++ [
-              appPkgs.fcitx5-gtk
-              appPkgs.libsForQt5.fcitx5-qt
-              appPkgs.qt6Packages.fcitx5-qt
-            ];
-          profile = (args.profile or "") + ''
-            export QT_IM_MODULE=fcitx
-            export SDL_IM_MODULE=fcitx
-            export XMODIFIERS=@im=fcitx
-          '';
-        }
-      );
-  };
-  wechat = pkgs.callPackage "${pkgs.path}/pkgs/by-name/we/wechat/package.nix" {
-    callPackage =
-      path: args:
-      pkgs.callPackage path (
-        args
-        // lib.optionalAttrs (builtins.baseNameOf (toString path) == "linux.nix") {
-          appimageTools = wechatAppimageTools;
-        }
-      );
-  };
-in
 {
   imports = [
     ./browser.nix
@@ -50,6 +15,7 @@ in
     ./office.nix
     ./terminal.nix
     ./wallpaper.nix
+    ./wechat.nix
     ./dms
   ];
 
@@ -59,8 +25,6 @@ in
       directory = ../../../../assets/wallpapers;
       initialStrategy = "random";
     };
-
-    home.packages = [ wechat ];
 
     xdg.userDirs = {
       enable = true;
