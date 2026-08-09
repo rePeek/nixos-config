d7628ec24a4fe9c8e9631092a0fd45d45ef5158c
rePeek <senxlin@gmail.com>
2026-08-09 16:26:55 +0800

    fix(bottles): handle nested tar extraction

--- modules/home-manager/desktop/general/bottles.nix
+++ modules/home-manager/desktop/general/bottles.nix
@@ -8,17 +8,21 @@ let
   bottles = pkgs.bottles.override {
     extraPkgs = _pkgs: [ pkgs.wineWow64Packages.stagingFull ];
 
-    bottles-unwrapped = pkgs.bottles-unwrapped.override {
-      python3Packages = pkgs.python3Packages.overrideScope (
-        _final: prev: {
-          # patool's MIME tests currently disagree with the file database result
-          # for compressed tar archives; this does not affect Bottles at runtime.
-          patool = prev.patool.overridePythonAttrs (_: {
-            doCheck = false;
-          });
-        }
-      );
-    };
+    bottles-unwrapped =
+      (pkgs.bottles-unwrapped.override {
+        python3Packages = pkgs.python3Packages.overrideScope (
+          _final: prev: {
+            # patool's MIME tests currently disagree with the file database result
+            # for compressed tar archives; this does not affect Bottles at runtime.
+            patool = prev.patool.overridePythonAttrs (_: {
+              doCheck = false;
+            });
+          }
+        );
+      }).overrideAttrs
+        (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [ ./bottles-inner-tar.patch ];
+        });
   };
 in
 {
