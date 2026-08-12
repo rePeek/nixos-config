f95177f576737b549346918ad7950c967643ac1c
rePeek <senxlin@gmail.com>
2026-08-12 11:01:45 +0800

    remove no used app

--- modules/home-manager/desktop/extra/default.nix
+++ modules/home-manager/desktop/extra/default.nix
@@ -1,21 +1,16 @@
-# Optional GUI applications for desktop Home Manager roles.
-# All apps are managed via Flatpak; packages here are merged
-# with the base list in general/flatpak.nix.
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   options.custom.desktop.extra.enable = lib.mkEnableOption "extra desktop applications";
   config = lib.mkIf config.custom.desktop.extra.enable {
+    home.packages = with pkgs; [
+      obs-studio
+    ];
     services.flatpak.packages = [
-      "cn.xfangfang.wiliwili"
-      "com.calibre_ebook.calibre"
-      "com.discordapp.Discord"
-      "com.obsproject.Studio"
-      "io.github.troyeguo.koodo-reader"
-      "org.qbittorrent.qBittorrent"
       "org.telegram.desktop"
     ];
   };
