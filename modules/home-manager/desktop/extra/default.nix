183629262af43c78ebd4d06d50f96ea69dfa9cb9
rePeek <senxlin@gmail.com>
2026-08-12 18:53:34 +0800

    fix: configure telegram flatpak integration

--- modules/home-manager/desktop/extra/default.nix
+++ modules/home-manager/desktop/extra/default.nix
@@ -13,5 +13,11 @@
     services.flatpak.packages = [
       "org.telegram.desktop"
     ];
+    services.flatpak.overrides."org.telegram.desktop".Context.filesystems = [
+      "xdg-download/Telegram Desktop:create"
+    ];
+    services.flatpak.overrides."org.telegram.desktop".Environment = {
+      QT_QPA_PLATFORMTHEME_QT6 = "xdgdesktopportal";
+    };
   };
 }
