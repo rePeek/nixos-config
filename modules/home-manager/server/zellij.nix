cd65cda1ed12d01364aa2b4be554e262ed0c7c3e
rePeek <senxlin@gmail.com>
2026-08-26 11:36:47 +0800

    fix: simplify zellij theme configuration

--- modules/home-manager/server/zellij.nix
+++ modules/home-manager/server/zellij.nix
@@ -1,67 +1,53 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
-let
-  stylixColors = lib.attrByPath [ "lib" "stylix" "colors" "withHashtag" ] null config;
-  stylixThemeEnabled = stylixColors != null;
-in
 {
   home.shellAliases = {
     zs = "zellij -s";
     za = "zellij a";
     zz = "zellij attach --create main";
-    zj = "zellij -l maximized";
+    zj = "zellij -l compact";
   };
 
   programs.zellij = {
     enable = true;
     package = pkgs.zellij;
-    settings =
-      lib.optionalAttrs stylixThemeEnabled {
-        theme = "stylix";
-      }
-      // {
-        support_kitty_keyboard_protocol = false;
-        pane_frames = true;
-        ui.pane_frames.rounded_corners = true;
-        simplified_ui = true;
-        default_layout = "maximized";
-        # misc
-        default_shell = "fish";
-        # keybind
-        keybinds =
-          with builtins;
-          let
-            binder =
-              bind:
-              let
-                keys = elemAt bind 0;
-                action = elemAt bind 1;
-                argKeys = map (k: "\"${k}\"") (lib.lists.flatten [ keys ]);
-              in
-              {
-                name = "bind ${concatStringsSep " " argKeys}";
-                value = action;
-              };
-            layer = binds: (listToAttrs (map binder binds));
-          in
-          {
-            locked = layer [
-              [
-                [ "Alt f" ]
-                { LaunchPlugin = "filepicker"; }
-              ]
-            ];
-          };
-      };
+    settings = {
+      support_kitty_keyboard_protocol = false;
+      pane_frames = true;
+      ui.pane_frames.rounded_corners = true;
+      simplified_ui = true;
+      default_layout = "compact";
+      # misc
+      default_shell = "fish";
+      # keybind
+      keybinds =
+        with builtins;
+        let
+          binder =
+            bind:
+            let
+              keys = elemAt bind 0;
+              action = elemAt bind 1;
+              argKeys = map (k: "\"${k}\"") (lib.lists.flatten [ keys ]);
+            in
+            {
+              name = "bind ${concatStringsSep " " argKeys}";
+              value = action;
+            };
+          layer = binds: (listToAttrs (map binder binds));
+        in
+        {
+          locked = layer [
+            [
+              [ "Alt f" ]
+              { LaunchPlugin = "filepicker"; }
+            ]
+          ];
+        };
+    };
 
-    layouts.maximized = ''
-      layout {
-        pane
-      }
-    '';
   };
 }
