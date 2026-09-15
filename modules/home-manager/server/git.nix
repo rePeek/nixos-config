90fdaca014743ae95de501e29f5a0ccc591d06b4
rePeek <senxlin@gmail.com>
2026-09-15 10:09:18 +0800

    fix(lazygit): wrap with TCELL_KEYBOARD_PROTOCOL=legacy to fix <esc> in Ghostty sub-views

--- modules/home-manager/server/git.nix
+++ modules/home-manager/server/git.nix
@@ -12,6 +12,18 @@
 
     lazygit = {
       enable = true;
+      # Wrap lazygit to force legacy keyboard protocol in Ghostty,
+      # fixing <esc> not working in sub-views (kitty keyboard protocol issue)
+      package = pkgs.symlinkJoin {
+        name = "lazygit";
+        paths = [ pkgs.lazygit ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/lazygit \
+            --set TCELL_KEYBOARD_PROTOCOL legacy
+        '';
+        meta.mainProgram = "lazygit";
+      };
       settings = {
         git = {
           # Improves performance
